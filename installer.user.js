1f21d91798775fa4c520d4bad0744165c7d5689f
π <cattlecattle8033@gmail.com>
2026-09-05 21:42:13 +0800

    Add user script for MT Baccarat statistics
    Initial creation of a user script for MT Baccarat statistics and analysis.

---
+++ installer.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         龍盈科技【MT百家樂】專業設計精巧版
+// @namespace    http://tampermonkey.net/
+// @version      61.0
+// @description  龍盈科技 MT 百家樂即時牌局統計與規則分析面板
+// @author       程式夥伴
+// @match        *://*.ofalive99.net/*
+// @match        *://gsa.ofalive99.net/*
+// @run-at       document-start
+// @noframes
+// @require      https://raw.githubusercontent.com/cattlecattle8033-gif/MT/main/MT_Baccarat_Source.js
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+})();
