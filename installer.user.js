4c1cd968af7b7d559a2c4601a8e211672683d568
π <cattlecattle8033@gmail.com>
2026-09-07 04:29:04 +0800

    龍盈研發團隊

--- installer.user.js
+++ installer.user.js
@@ -1,15 +1,20 @@
 // ==UserScript==
-// @name         龍盈科技【MT百家樂】專業設計精裝版
-// @namespace    http://tampermonkey.net
+// @name         龍盈科技【MT百家樂】
+// @namespace    http://tampermonkey.net/
 // @version      62.0.0
-// @description  龍盈科技 MT 百家樂即時局勢統計與高階分析面板
-// @author       龍盈研發團隊
+// @description  龍盈科技 MT 百家樂即時局勢統計、凱利公式動態注碼與高階路則分析面板
+// @author       
 // @match        *://*.ofalive99.net/*
 // @match        *://gsa.ofalive99.net/*
 // @run-at       document-start
 // @grant        none
 // @noframes
-// @updateURL    https://githubusercontent.com
-// @downloadURL  https://githubusercontent.com
-// @require      https://githubusercontent.com
-// ==UserScript==
+// @updateURL    https://raw.githubusercontent.com/cattlecattle8033-gif/MT/main/installer.user.js
+// @downloadURL  https://raw.githubusercontent.com/cattlecattle8033-gif/MT/main/installer.user.js
+// @require      https://raw.githubusercontent.com/cattlecattle8033-gif/MT/main/MT_Baccarat_Source.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('%c[龍盈科技]%c 頂級量子集成計算與動態防禦模組已成功載入。', 'color: #e0474c; font-weight: bold;', 'color: default;');
+})();
